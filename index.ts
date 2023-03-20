f0fdb17935e3f9c92de11a5e889d74e1b25a3e1f
mic-dem <michal.demski@interia.pl>
2023-03-20 22:42:29 +0100

    Change index

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 import express, {json} from "express";
 import cors from "cors"
 import 'express-async-errors';
+import rateLimit from 'express-rate-limit';
 import {handleError} from "./util/errors";
+import {mathRouter} from "./routers/math-router";
+import {config} from "./config/config.exemple";
 
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: config.corsOrigin,
 }));
 app.use(json());
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 3700,
+}));
+
+app.use('/math', mathRouter);
 
 
 app.use(handleError);
